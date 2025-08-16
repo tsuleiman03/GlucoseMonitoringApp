40eae1b2d3005dcd2fab2c1abb3ef32a2e89daed
Tunde Suleiman <tsuleiman03@users.noreply.github.com>
2025-08-16 13:03:47 +0100

    Clean-up: Add version header to constants/Colors.ts

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,3 +1,9 @@
+// constants/Colors.ts
+
+// Version: 1.0.0
+// Date: 2025-08-16
+// Changes: Initial versioning of the operational color scheme file.
+
 /**
  * Below are the colors that are used in the app. The colors are defined in the light and dark mode.
  * There are many other ways to style your app. For example, [Nativewind](https://www.nativewind.dev/), [Tamagui](https://tamagui.dev/), [unistyles](https://reactnativeunistyles.vercel.app), etc.
